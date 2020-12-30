968f827011a563cff46cde05b5b2d3b6110f2260
Max Horn <max.horn@bsse.ethz.ch>
2020-12-30 09:30:07 +0100

    Fix multiple minor issues in preliminary implementation:
     - Incorrect computation of number of features for Constrained and
       Identity layer
     - Incorrect dim_head computation in RelPosSelfAttention
     - Add short test with random data

--- relative_performer/constrained_relative_encoding.py
+++ relative_performer/constrained_relative_encoding.py
@@ -48,7 +48,7 @@ class ConstrainedLinear(nn.Module):
     access to is purely relative.
     """
 
-    def __init__(self, in_features, out_features, n_pos_lengthscales_in, heads,
+    def __init__(self, in_features, out_features, pos_scales, heads,
                  bias=True):
         """Initialize ConstrainedLinear layer.
 
@@ -56,7 +56,7 @@ class ConstrainedLinear(nn.Module):
             dim_in: Dimensionality of the input elements.
             dim_out: Dimensionality of the output (excluding the dimensions
                 corresponding to the positional encoding).
-            n_pos_lengthscales: Number of sin/cos pairs with same lengthscale
+            n_pos_scales: Number of sin/cos pairs with same lengthscale
                 in the positional encoding.
             heads: Number of heads.
             bias: Include a bias.
@@ -65,22 +65,23 @@ class ConstrainedLinear(nn.Module):
 
         self.in_features = in_features
         self.out_features = out_features
-        self.n_pos_lengthscales_in = n_pos_lengthscales_in
+        self.pos_scales = pos_scales
         self.heads = heads
-        self.positional_features = (
-            n_pos_lengthscales_in*heads)
+        # Number of features per head
+        features_head = out_features // heads
+        positional_features_head = 2*pos_scales
 
         self.content_weight = nn.Parameter(
             torch.Tensor(out_features, in_features))
         self.alpha = nn.Parameter(
-            torch.Tensor(self.positional_features))
+            torch.Tensor(pos_scales*heads))
         self.beta = nn.Parameter(
-            torch.Tensor(self.positional_features))
+            torch.Tensor(pos_scales*heads))
         self.register_buffer(
             'offdiag_matrix', torch.Tensor([[0., 1.], [-1., 0.]]))
         if bias:
-            self.bias = nn.Parameter(torch.Tensor(
-                self.out_features+self.positional_features))
+            self.bias = nn.Parameter(torch.empty(
+                1, heads, 1, features_head+positional_features_head))
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
@@ -98,19 +99,20 @@ class ConstrainedLinear(nn.Module):
         """Build projection matrix for positional encodings.
 
         Returns:
-            Block diagonal matrix with shape [heads, n_pos_lengthscales,
-            n_pos_lengthscales].
+            Block diagonal matrix with shape [heads, pos_scales,
+            pos_scales].
         """
         device = self.alpha.device
+        n_parameters = self.alpha.numel()
         identity_matrix = torch.diag(torch.ones(2, device=device))
         matrices = (
-            self.alpha[:, None] * identity_matrix[None, :]
-            + self.beta[:, None] * self.offdiag_matrix[None, :]
+            self.alpha[:, None, None] * identity_matrix[None]
+            + self.beta[:, None, None] * self.offdiag_matrix[None]
         )
         return torch.stack([
-            torch.block_diag(*matrices[i:i+(1*self.n_pos_lengthscales_in)])
+            torch.block_diag(*matrices[i:i+(1*self.pos_scales)])
             for i in range(
-                0, self.positional_features, self.n_pos_lengthscales_in)
+                0, n_parameters, self.pos_scales)
         ], axis=0)
 
     def forward(self, input: torch.Tensor, pos_encodings: torch.Tensor):
@@ -135,7 +137,7 @@ class IdentityLinear(nn.Module):
     access to is purely relative.
     """
 
-    def __init__(self, in_features, out_features, n_pos_lengthscales_in, heads,
+    def __init__(self, in_features, out_features, pos_scales, heads,
                  bias=True):
         """Initialize IdentityLinear layer.
 
@@ -152,16 +154,17 @@ class IdentityLinear(nn.Module):
 
         self.in_features = in_features
         self.out_features = out_features
-        self.n_pos_lengthscales_in = n_pos_lengthscales_in
+        self.pos_scales = pos_scales
         self.heads = heads
-        self.positional_features = (
-            n_pos_lengthscales_in*heads)
+        # Number of features per head
+        features_head = out_features // heads
+        positional_features_head = 2*pos_scales
 
         self.content_weight = nn.Parameter(
             torch.Tensor(out_features, in_features))
         if bias:
-            self.bias = nn.Parameter(torch.Tensor(
-                self.out_features+self.positional_features))
+            self.bias = nn.Parameter(torch.empty(
+                1, heads, 1, features_head+positional_features_head))
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
@@ -183,15 +186,14 @@ class IdentityLinear(nn.Module):
 
 
 class RelPosSelfAttention(nn.Module):
-    def __init__(self, dim, causal=False, heads=8, dim_head=64,
-                 pos_lengthscales=4, nb_features=None,
-                 feature_redraw_interval=1000, generalized_attention=False,
-                 kernel_fn=nn.ReLU(), qr_uniform_q=False, dropout=0.,
-                 no_projection=False):
+    def __init__(self, dim, causal=False, heads=8, pos_scales=4,
+                 nb_features=None, feature_redraw_interval=1000,
+                 generalized_attention=False, kernel_fn=nn.ReLU(),
+                 qr_uniform_q=False, dropout=0., no_projection=False):
         super().__init__()
         assert dim % heads == 0, 'dimension must be divisible by number of heads'
-        dim_head = default(dim_head, dim // heads)
-        inner_dim = dim_head * heads
+        dim_head = dim // heads + 2*pos_scales
+        inner_dim = dim
         self.fast_attention = FastAttention(
             dim_head,
             nb_features,
@@ -204,8 +206,10 @@ class RelPosSelfAttention(nn.Module):
 
         self.heads = heads
 
-        self.to_q = ConstrainedLinear(dim, inner_dim)
-        self.to_k = IdentityLinear(dim, inner_dim)
+        self.to_q = ConstrainedLinear(
+            dim, inner_dim, pos_scales, self.heads)
+        self.to_k = IdentityLinear(
+            dim, inner_dim, pos_scales, self.heads)
         self.to_v = nn.Linear(dim, inner_dim)
         self.to_out = nn.Linear(inner_dim, dim)
         self.dropout = nn.Dropout(dropout)
@@ -219,7 +223,7 @@ class RelPosSelfAttention(nn.Module):
         context_mask = default(
             context_mask, mask) if not cross_attend else context_mask
 
-        q, k, v = self.to_q(x), self.to_k(context), self.to_v(context)
+        q, k, v = self.to_q(x, pos), self.to_k(context, pos), self.to_v(context)
 
         # q and k are already in the right format
         v = rearrange(v, 'b n (h d) -> b h n d', h=h)
@@ -233,3 +237,84 @@ class RelPosSelfAttention(nn.Module):
         out = rearrange(out, 'b h n d -> b n (h d)')
         out = self.to_out(out)
         return self.dropout(out)
+
+
+class RelativePerformer(nn.Module):
+    def __init__(self, dim, depth, heads, pos_dims=1, pos_scales=4, causal = False, ff_mult = 4, nb_features = None, feature_redraw_interval = 1000, reversible = False, ff_chunks = 1, generalized_attention = False, kernel_fn = nn.ReLU(), qr_uniform_q = False, use_scalenorm = False, use_rezero = False, ff_glu = False, ff_dropout = 0., attn_dropout = 0., cross_attend = False, no_projection = False):
+        super().__init__()
+        self.positional_encoding = LearnableSinusoidEncoding(pos_scales*2)
+        layers = nn.ModuleList([])
+
+        if use_scalenorm:
+            wrapper_fn = partial(PreScaleNorm, dim)
+        elif use_rezero:
+            wrapper_fn = ReZero
+        else:
+            wrapper_fn = partial(PreLayerNorm, dim)
+
+        for _ in range(depth):
+            layers.append(nn.ModuleList([
+                wrapper_fn(
+                    RelPosSelfAttention(
+                        dim, causal=causal, heads=heads,
+                        pos_scales=pos_dims*pos_scales,
+                        nb_features=nb_features,
+                        generalized_attention=generalized_attention,
+                        kernel_fn=kernel_fn,
+                        qr_uniform_q=qr_uniform_q,
+                        dropout=attn_dropout,
+                        no_projection=no_projection)),
+                wrapper_fn(
+                    Chunk(
+                        ff_chunks,
+                        FeedForward(dim, mult=ff_mult, dropout=ff_dropout, glu=ff_glu),
+                        along_dim=1)
+                )
+            ]))
+
+        execute_type = ReversibleSequence if reversible else SequentialSequence
+
+        route_attn = ((True, False),) * depth * (2 if cross_attend else 1)
+        route_positions = ((True, False),) * depth * (2 if cross_attend else 1)
+        route_context = ((False, False), (True, False)) * depth
+        attn_route_map = {'mask': route_attn}
+        positions_route_map = {'pos': route_positions}
+        context_route_map = {'context': route_context, 'context_mask': route_context} if cross_attend else {}
+        self.net = execute_type(layers, args_route={**attn_route_map, **context_route_map, **positions_route_map})
+
+        # keeping track of when to redraw projections for all attention layers
+        self.feature_redraw_interval = feature_redraw_interval
+        self.register_buffer('calls_since_last_redraw', torch.tensor(0))
+
+    def fix_projection_matrices_(self):
+        self.feature_redraw_interval = None
+
+    def check_redraw_projections(self):
+        if not self.training:
+            return
+
+        if exists(self.feature_redraw_interval) and self.calls_since_last_redraw >= self.feature_redraw_interval:
+            device = get_module_device(self)
+
+            fast_attentions = find_modules(self, FastAttention)
+            for fast_attention in fast_attentions:
+                fast_attention.redraw_projection_matrix(device)
+
+            self.calls_since_last_redraw.zero_()
+            return
+
+        self.calls_since_last_redraw += 1
+
+    def forward(self, x, positions, **kwargs):
+        self.check_redraw_projections()
+        pos = rearrange(
+            self.positional_encoding(positions), 'b n p d -> b n (p d)')
+        return self.net(x, pos=pos, **kwargs)
+
+if __name__ == '__main__':
+    model = RelativePerformer(
+        dim=512, depth=1, heads=8, pos_dims=1, pos_scales=4, causal=False
+    )
+    positions = torch.arange(1024, dtype=torch.float32)[None, :, None]
+    x = torch.randn(1, 1024, 512)
+    model(x, positions) # (1, 2048, 512)
