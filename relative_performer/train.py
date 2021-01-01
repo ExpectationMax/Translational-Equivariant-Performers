d21af480e0b992ffa70336a35b5d18f298790e76
Max Horn <max.horn@bsse.ethz.ch>
2021-01-01 16:05:34 +0100

    Refactor train.py such that both Performer and RelativePerformer work

--- relative_performer/train.py
+++ relative_performer/train.py
@@ -8,6 +8,7 @@ import pytorch_lightning as pl
 import pl_bolts.datamodules as datasets
 from einops import rearrange
 
+from relative_performer.performer_pytorch import Performer
 from relative_performer.constrained_relative_encoding import (
     RelativePerformer, LearnableSinusoidEncoding)
 
@@ -15,34 +16,28 @@ GPU_AVAILABLE = torch.cuda.is_available() and torch.cuda.device_count() > 0
 DATA_PATH = Path(__file__).parent.parent.joinpath('data')
 
 
-class RelativePerformerModel(pl.LightningModule):
-    def __init__(self, dim, depth, heads, pos_scales, in_features=1,
-                 pos_dims=1, max_pos=32, num_classes=10, **kwargs):
+class PerfomerBase(pl.LightningModule):
+    """Base class of Performer models for image classification."""
+
+    positional_embedding: nn.Module
+
+    def __init__(self, in_features, dim, num_classes, **kwargs):
         super().__init__()
-        self.save_hyperparameters()
-        self.positional_embedding = LearnableSinusoidEncoding(
-            pos_scales*2, max_timescale_init=max_pos*2)
+        self.in_features = in_features
+        self.dim = dim
+        self.num_classes = num_classes
+
         self.content_embedding = nn.Linear(in_features, dim)
         self.class_query = nn.Parameter(torch.Tensor(dim))
-        self.performer = RelativePerformer(
-            dim,
-            depth,
-            heads,
-            pos_dims=pos_dims,
-            pos_scales=pos_scales
-        )
         self.output_layer = nn.Linear(dim, num_classes)
         self.loss = nn.CrossEntropyLoss()
-        self.reset_parameters()
 
         self.train_acc = pl.metrics.Accuracy()
         self.val_acc = pl.metrics.Accuracy()
+        self.reset_parameters()
 
     def reset_parameters(self) -> None:
         nn.init.normal_(self.class_query)
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), self.hparams.learning_rate)
 
     def _flatten_to_sequence(self, input: torch.Tensor):
         """Flatten the 2D input into a 1D sequence.
@@ -101,6 +96,87 @@ class RelativePerformerModel(pl.LightningModule):
         )
         return embedding, pos_embedding
 
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        # The datasets always input in the format (C, W, H) instead of (W, H,
+        # C).
+        x = x.permute(0, 2, 3, 1)
+        logits = self(x)
+        loss = self.loss(logits, y)
+        self.log('train_loss', loss, on_step=True, on_epoch=True)
+        self.train_acc(logits, y)
+        self.log('train_acc', self.train_acc, on_step=True, on_epoch=True,
+                 prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        # The datasets always input in the format (C, W, H) instead of (W, H,
+        # C).
+        x = x.permute(0, 2, 3, 1)
+        logits = self(x)
+        loss = self.loss(logits, y)
+        self.log('val_loss', loss, on_step=True, on_epoch=True)
+        self.val_acc(logits, y)
+        self.log('val_acc', self.val_acc, on_step=True, on_epoch=True)
+        return loss
+
+
+class PerformerModel(PerfomerBase):
+    def __init__(self, dim, depth, heads, max_pos=32, **kwargs):
+        super().__init__(dim=dim, **kwargs)
+        self.save_hyperparameters()
+        self.positional_embedding = LearnableSinusoidEncoding(
+            dim // 2, max_timescale_init=max_pos*1000)
+        self.performer = Performer(
+            dim,
+            depth,
+            heads
+        )
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), self.hparams.learning_rate)
+
+    def forward(self, x):
+        embedding = self.content_embedding(x)
+        embedding, positions = self._flatten_to_sequence(embedding)
+        positions = self._compute_positional_embeddings(positions)
+        embedding, positions = self._add_class_query(embedding, positions)
+        embedding += positions
+        del positions
+        # First element contains class prediction
+        out = self.performer(embedding)[:, 0]
+        return self.output_layer(out)
+
+    @ staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = argparse.ArgumentParser(
+            parents=[parent_parser], add_help=False)
+        parser.add_argument('--learning_rate', default=0.001, type=float)
+        parser.add_argument('--dim', type=int, default=128)
+        parser.add_argument('--depth', type=int, default=4)
+        parser.add_argument('--heads', type=int, default=4)
+        return parser
+
+
+class RelativePerformerModel(PerfomerBase):
+    def __init__(self, dim, depth, heads, pos_scales, pos_dims=1, max_pos=32,
+                 **kwargs):
+        super().__init__(dim=dim, **kwargs)
+        self.save_hyperparameters()
+        self.positional_embedding = LearnableSinusoidEncoding(
+            pos_scales*2, max_timescale_init=max_pos*2)
+        self.performer = RelativePerformer(
+            dim,
+            depth,
+            heads,
+            pos_dims=pos_dims,
+            pos_scales=pos_scales
+        )
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), self.hparams.learning_rate)
+
     def forward(self, x):
         embedding = self.content_embedding(x)
         embedding, positions = self._flatten_to_sequence(embedding)
@@ -149,15 +225,22 @@ class RelativePerformerModel(pl.LightningModule):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('model', choices=['Performer', 'RelativePerformer'])
     parser.add_argument('dataset', choices=[
         'FashionMNIST', 'MNIST', 'CIFAR10'])
     parser.add_argument('--batch_size', default=16, type=int)
 
-    parser = RelativePerformerModel.add_model_specific_args(parser)
+    partial_args, _ = parser.parse_known_args()
+    if partial_args.model == 'Performer':
+        parser = PerformerModel.add_model_specific_args(parser)
+        model = PerformerModel
+    elif partial_args.model == 'RelativePerformer':
+        parser = RelativePerformerModel.add_model_specific_args(parser)
+        model = RelativePerformerModel
     args = parser.parse_args()
 
     data_cls = getattr(datasets, args.dataset + 'DataModule')
-    num_workers = 4
+    num_workers = 0
     # Handle incosistencies in DataModules: Some datasets accept batch_size,
     # some don't, some simply ignore it.
     if args.dataset == 'MNIST':
@@ -181,7 +264,7 @@ if __name__ == '__main__':
 
     in_features, nx, ny = dataset.dims
     max_pos = max(nx, ny)
-    model = RelativePerformerModel(
+    model = model(
         **vars(args),
         in_features=in_features,
         pos_dims=2,
@@ -189,7 +272,8 @@ if __name__ == '__main__':
         num_classes=dataset.num_classes
     )
 
-    trainer = pl.Trainer(gpus=-1 if GPU_AVAILABLE else None)
+    trainer = pl.Trainer(gpus=-1 if GPU_AVAILABLE else None,
+                         weights_summary='full')
     # Handle incosistencies in DataModules: Some datasets only listen to the
     # batch_size argumen if it is passed here, others don't have to argument.
     # MNIST and FashionMNIST take batch_size as argument here, while CIFAR10
