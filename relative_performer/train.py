60fcd6d3fdceb7518f31ca61462a9df44c2fb1d6
Max Horn <max.horn@bsse.ethz.ch>
2021-01-04 14:17:24 +0100

    Use 4 workers for jobs

--- relative_performer/train.py
+++ relative_performer/train.py
@@ -347,8 +347,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     data_cls = getattr(datasets, args.dataset + 'DataModule')
-    num_workers = 0
-
+    num_workers = 4
     transforms = ToIntTensor if args.embedding_type == 'lookup' else None
     # Handle incosistencies in DataModules: Some datasets accept batch_size,
     # some don't, some simply ignore it.
